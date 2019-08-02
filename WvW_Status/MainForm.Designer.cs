d2cb97babc5759ef38ebae6118745df2d7e228db
GorillaNuggets <53388340+GorillaNuggets@users.noreply.github.com>
2019-08-01 23:07:45 -0400

    Added Tooltips to Pictureboxes

--- WvW_Status/MainForm.Designer.cs
+++ WvW_Status/MainForm.Designer.cs
@@ -37,7 +37,7 @@
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(234, 212);
             this.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ForeColor = System.Drawing.Color.White;
+            this.ForeColor = System.Drawing.Color.Gainsboro;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
