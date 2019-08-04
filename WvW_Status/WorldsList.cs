41a629a4899a78bbca1176ff22901efb0244df96
GorillaNuggets <53388340+GorillaNuggets@users.noreply.github.com>
2019-08-04 03:02:40 -0400

    Removed Skirmishes Remaining and added End of Skirmish Refactored code and added Refresh button

--- WvW_Status/WorldsList.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WvW_Status
-{
-    public class WorldsList
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Population { get; set; }
-    }
-}
